5831ef4147cf3dfd67cee6ba76c5503a17e3f6c5
Benjamin Graham <benjisgraham@gmail.com>
2018-09-26 16:06:27 -0500

    Another quick change

--- MVC Test App/Controllers/HomeController.cs
+++ MVC Test App/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVC_Test_App.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,12 @@ namespace MVC_Test_App.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult ViewEmployees()
+        {
+            var vm = new EmployeeViewModel();
+            return View(vm);
         }
 
 
