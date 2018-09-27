81b241bfc5d5bec9f646dee99606531b27da8a66
Benjamin Graham <benjisgraham@gmail.com>
2018-09-27 08:32:35 -0500

    Stuff

--- MVC Test App/Controllers/HomeController.cs
+++ MVC Test App/Controllers/HomeController.cs
@@ -25,10 +25,42 @@ namespace MVC_Test_App.Controllers
         {
             var vm = new EmployeeViewModel();
             vm.employee = vm.employeeList.SingleOrDefault(i => i.ID == id);
-            return View(vm);
+            return View(vm.employee);
                 
         }
 
+        public ActionResult Edit(EmployeeViewModel vm, int id)
+        {
+            var employee = vm.employeeList.SingleOrDefault(i => i.ID == id);
+            return View(employee);
+        }
+
+        public ActionResult Create()
+        {
+            
+            return View("NewEmployee");
+        }
+
+        [HttpPost]
+        public ActionResult Create(EmployeeViewModel vm)
+        {
+            vm.employeeList.Add(new Employee(
+                vm.employeeList.Count + 1,
+                vm.employee.FirstName,
+                vm.employee.LastName,
+                vm.employee.EmpStatus,
+                vm.employee.Department,
+                vm.employee.PhoneNum,
+                vm.employee.Email,
+                vm.employee.Address1,
+                vm.employee.Address2,
+                vm.employee.City,
+                vm.employee.State,
+                vm.employee.Zip
+                ));
+
+            return RedirectToAction("EmployeeDetails", vm.employeeList.Last().ID);
+        }
 
     }
 }
